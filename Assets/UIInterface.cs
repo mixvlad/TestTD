491b17cda76541db251b75d671f87059d48a0ed8
mixvlad <mixa@xakep.ru>
2025-05-29 18:06:26 +0300

    Finish 4 part

--- Assets/UIInterface.cs
+++ Assets/UIInterface.cs
@@ -1,11 +1,50 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
 public class UIInterface : MonoBehaviour
 {
-    public GameObject tower;
+    public GameObject rocket;
+    public GameObject gattling;
+    public GameObject flamer;
+    
+    public GameObject turretMenu;
+    GameObject itemPrefab;
     GameObject focusObj;
     private Camera mainCamera;
+
+    public void CreateRocket()
+    {
+        itemPrefab = rocket;
+        CreateItem();
+    }
+    public void CreateFlamer()
+    {
+        itemPrefab = flamer;
+        CreateItem();
+    }
+
+    public void CreateGatling()
+    {
+        itemPrefab = gattling;
+        CreateItem();
+    }
+
+    public void CloseTurretMenu()
+    {
+        turretMenu.SetActive(false);
+    }
+
+    void CreateItem()
+    {
+        RaycastHit hit;
+        Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+        if (Physics.Raycast(ray, out hit))
+        {
+            focusObj = Instantiate(itemPrefab, hit.point, itemPrefab.transform.rotation);
+            focusObj.GetComponent<Collider>().enabled = false;
+        }
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,20 +57,43 @@ public class UIInterface : MonoBehaviour
     {
         if (Mouse.current.leftButton.wasPressedThisFrame)
         {
+            if(EventSystem.current.IsPointerOverGameObject())
+            {
+                return;
+            }
+            RaycastHit hit;
+            Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+            if (Physics.Raycast(ray, out hit) && hit.collider.gameObject.CompareTag("turret"))
+            {
+                turretMenu.transform.position = Mouse.current.position.ReadValue();
+                turretMenu.SetActive(true);
+            }
+        }
+        else if (focusObj != null && Mouse.current.leftButton.isPressed)
+        {
             RaycastHit hit;
             Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
             if (Physics.Raycast(ray, out hit))
             {
-                focusObj = Instantiate(tower, hit.point, tower.transform.rotation);
+                focusObj.transform.position = hit.point;
             }
         }
-        else if (Mouse.current.leftButton.isPressed)
+        else if (focusObj != null && Mouse.current.leftButton.wasReleasedThisFrame)
         {
-            
-        }
-        else if (Mouse.current.leftButton.wasReleasedThisFrame)
-        {
-            
+            RaycastHit hit;
+            Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+            if (Physics.Raycast(ray, out hit) && hit.collider.gameObject.CompareTag("platform") && hit.normal == new Vector3(0, 1, 0))
+            {
+                hit.collider.gameObject.tag = "occupied";
+                focusObj.transform.position = hit.collider.gameObject.transform.position + new Vector3(0, hit.collider.gameObject.transform.localScale.y / 2, 0);
+                focusObj.GetComponent<Collider>().enabled = true;
+            }
+            else
+            {
+                Destroy(focusObj);
+            }
+
+            focusObj = null;
         }
     }
 }
